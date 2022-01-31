176327f50300ecbe4a4f6ffd4e5924c2df23ddf5
Arthur <96789842+ArthurReshetniak@users.noreply.github.com>
2022-01-31 20:31:55 +0200

    Add files via upload

---
+++ JS4.js
@@ -0,0 +1,35 @@
+
+const age = 60;
+
+if (age>= 55 && age<= 99){
+    console.log('Попало');
+} else {
+    console.log('Не попало');
+}
+
+
+const age = 50;
+let user;
+
+if (age <=16) {
+    user = Дитина;
+} else if (age > 16 && age < 61){
+    user = Зрослый;
+} else {
+    user = Пенсионер;
+}
+
+console.log(user);
+
+
+const name1 = '';
+const surname = '';
+if (name1.length > 4 && surname.length > 5) {
+    alert(name1.length + surname.length);
+} else {
+    alert("Упс")
+}
+
+let leng = "en";
+let month;
+
